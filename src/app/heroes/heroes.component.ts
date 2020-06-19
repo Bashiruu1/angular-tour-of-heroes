f0a5d5819dc2c7bf9e9de506174c60959c5533bd
Bashiruu1 <silvergold21@gmail.com>
2020-06-19 00:18:11 -0400

    added hero component and IHero interface

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { IHero } from '../interfaces/Ihero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  hero: IHero = {
+    id: 1,
+    name: "Windstorm"
+  };
+  constructor() { 
+    
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+}
